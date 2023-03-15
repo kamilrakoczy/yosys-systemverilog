0028ee7d00169cf685d6f2ca9221b8d4de77e037
Kamil Rakoczy <kamilrakoczy1@gmail.com>
2023-03-15 14:30:48 +0100

    WIP: test automatic logs update (#1)
    * Add initial logs
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>
    
    * WIP: CI update permissions
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>
    
    * Automatically update logs when they changed
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>
    
    * Update CI script
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>
    
    * Set +x
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>
    
    ---------
    
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

---
+++ .github/scripts/ci_github_update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -u -o pipefail
+shopt -s nullglob
+shopt -s extglob
+
+
+if [ $# -ne 3 ]; then
+    echo "Wrong number of arguments"
+    echo "  expected: $0 <path> <commit message> <remote ref>"
+    exit 1
+fi
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
+if [ -n "$(git status $1 --porcelain)" ]; then
+  git add $1
+  git commit -m $2
+  git push --set-upstream origin $3
+fi
